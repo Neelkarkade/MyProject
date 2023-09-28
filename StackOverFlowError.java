83983dfea7b52fefe7a4ba72e8df18e6baf6446c
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-09-28 23:29:50 +0530

    Stack Over Floow Error

---
+++ StackOverFlowError.java
@@ -0,0 +1,12 @@
+package exception_handling;
+
+public class StackOverFlowError {
+	StackOverFlowError () {
+		StackOverFlowError  a2 = new StackOverFlowError();
+	}
+		public static void main(String[]args) {
+			StackOverFlowError a1 = new StackOverFlowError();
+	
+	
+}
+}
