1c5856885f0a37c8add537871d13436ee4fe29c9
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-03 01:57:15 +0530

    NumberFormatException

---
+++ NumberFormatException.java
@@ -0,0 +1,24 @@
+package exception_handling;
+
+public class NumberFormatException {
+	public static void main(String[] args) {
+		try {
+			String x = "abc123";
+			int x1 = Integer.parseInt(x);
+			System.out.println(x1);
+			
+			float x2 = Float.parseFloat(x);
+			System.out.println(x2);
+			
+			long x3 = Long.parseLong(x);
+			System.out.println(x3);
+			
+			double x4 = Double.parseDouble(x);
+			System.out.println(x4);
+		}catch (Exception e) {
+			e.printStackTrace();
+	}
+
+	
+}
+}
