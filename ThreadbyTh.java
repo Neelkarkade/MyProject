544369b5df5162f99f33d6b8c961ce1d2ce4d560
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-25 23:52:51 +0530

    multithreading

---
+++ ThreadbyTh.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+public class ThreadbyTh extends Thread{
+	
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("I am ThreadbyThread");
+		}
+	}
+	public static void main(String[] args) {
+		}
+
+}
