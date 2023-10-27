6ecfa322298060e5222d0b402ecbe0ca419ee91a
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-28 02:09:16 +0530

    Do_While_Loop

---
+++ Do_While_LoopEx6.java
@@ -0,0 +1,12 @@
+package Do_While_Loop;
+
+public class Do_While_LoopEx6 {
+	public static void main(String[] args) {
+		
+		int x = 0;
+		do{
+			System.out.println(x);
+			x++;
+		}while(x < 3);
+		}
+}
