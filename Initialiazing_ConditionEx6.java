c45c62ed50bc55cee4637119fefb891e859c0840
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-30 23:33:28 +0530

    Do_While_Loop

---
+++ Initialiazing_ConditionEx6.java
@@ -0,0 +1,15 @@
+package Do_While_Loop;
+public class Initialiazing_ConditionEx6 {
+public static void main(String[] args) {
+	int i = 12;
+	while(i < 11) {
+		System.out.println("Apna Coolege");//run it  is not anything Because( i < 11 )Greater than (1 = 12)
+	}
+	do {
+		System.out.println("Apna Coolege");
+	}while(i < 11);
+	
+  }
+}
+  
+
