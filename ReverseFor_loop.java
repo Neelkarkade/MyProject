fedcf27728da9e50a3dd1a705b9d0894c5151b55
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-31 23:17:10 +0530

    Do_While_Loop

---
+++ ReverseFor_loop.java
@@ -0,0 +1,9 @@
+package Do_While_Loop;
+
+public class ReverseFor_loop {
+public static void main(String[] args) {// Reverse  in for loop
+	for (int i = 10; i> 0; i--) {//(i = 10,9,8,7,6,5,4,3,2,1)Negative sign then will value go from Higher number to Lower Number
+		System.out.println(i);
+	}
+}
+}
