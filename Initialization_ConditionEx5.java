59ac31016ff867f37b2bdabd11e9e44f8ab045a3
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-10-31 23:16:48 +0530

    Do_While_Loop

---
+++ Initialization_ConditionEx5.java
@@ -0,0 +1,12 @@
+package Do_While_Loop;
+
+public class Initialization_ConditionEx5 {
+public static void main(String[] args) {
+	int i = 0;
+	do {
+		System.out.println(i);
+		i = i+1;
+	}while(i < 11);
+	}
+}
+
