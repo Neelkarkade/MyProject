0eb9ae408f186a9d26cd69f8f32fe2680ecd4d72
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-11-01 23:53:56 +0530

    Ladder_Else_If

---
+++ Ladder_Else_IfEx7.java
@@ -0,0 +1,16 @@
+package Ladder_else_if_Statement;
+
+public class Ladder_Else_IfEx7 {
+public static void main(String[] args) {
+	int marks = 90;//(90 Double  Equal 90 then get (3))
+		if(marks == 70) {
+			System.out.println(1);
+		}else if (marks == 80) {
+			System.out.println(2);
+		}else if (marks == 90) {
+			System.out.println(3);
+		}else {
+		System.out.println(4);
+	}
+}
+}
