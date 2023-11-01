8fa6c204843ca0648301c872613bb0fd4cf68e3d
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-11-01 23:58:12 +0530

    Loop

---
+++ Loop.java
@@ -0,0 +1,9 @@
+package loops;
+public class Loop {
+	public static void main(String args[]) {
+		//counter++ => counter = counter +1
+		for(int counter = 0; counter < 11; counter++) {
+			System.out.println(counter + "     " );
+	  }
+	 }
+	}
