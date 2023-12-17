1469376b92ef461aa5805fa53dcbe503f9c9609b
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-17 23:46:10 +0530

    RegistrationService

---
+++ RegistrationService.java
@@ -0,0 +1,17 @@
+package com.webapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webapp.entity.Registration;
+import com.webapp.repository.RegistrationRepository;
+
+@Service
+public class RegistrationService {
+@Autowired
+private RegistrationRepository registrationRepository;
+
+public void saveRegistration(Registration registration) {
+	registrationRepository.save(registration);
+}
+}
