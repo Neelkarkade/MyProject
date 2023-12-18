21baff41b8d4e2fc4e496f2dfaaf6f75d535eea4
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-18 23:44:31 +0530

    <th>First Name</th>

--- RegistrationController.java
+++ RegistrationController.java
@@ -2,8 +2,12 @@ package com.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.webapp.dto.RegistrationDto;
 import com.webapp.entity.Registration;
 import com.webapp.service.RegistrationService;
 
@@ -20,16 +24,53 @@ public class RegistrationController {
 	
 		return "new_registration";
 	}
-	@RequestMapping("/saveReg")
-	public String saveRegistration(Registration registration) {
+	//@RequestMapping("/saveReg")
+	//public String saveRegistration(@ModelAttribute Registration  registration) {
 		//System.out.println(registration.getFirstName());
 		//System.out.println(registration.getLastName());
 	//	System.out.println(registration.getEmail());
 	//	System.out.println(registration.getMobile());
+		//registrationService.saveRegistration(registration);
+	//	return"new_registration";
+	//}
+//	@RequestMapping("/saveReg")
+//	public String saveRegistration(
+//			@RequestParam("firstName")String fName,
+//			@RequestParam("lastName")String lName,
+//			@RequestParam("email")String email,
+//			@RequestParam("mobile")long mobile,
+//			ModelMap model
+
+			
+//			) {
+		
+ //        Registration registration = new Registration();
+ //        registration.setFirstName(fName);
+  //       registration.setLastName(lName);
+//         registration.setEmail(email);
+//         registration.setMobile(mobile);
+
+//		registrationService.saveRegistration(registration);
+//		model.addAttribute("msg","Record is saved!!");
+//		return"new_registration";
+//	}
+	@RequestMapping("/saveReg")
+	public String saveRegistration(
+			
+			RegistrationDto dto,
+			ModelMap model
+
+			
+			) {
+		
+         Registration registration = new Registration();
+         registration.setFirstName(dto.getFirstName());
+         registration.setLastName(dto.getLastName());
+         registration.setEmail(dto.getEmail());
+         registration.setMobile(dto.getMobile());
+
 		registrationService.saveRegistration(registration);
+		model.addAttribute("msg","Record is saved!!");
 		return"new_registration";
 	}
-	
-	
-	
 }
