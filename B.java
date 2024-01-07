9705212d917bd097f51260fc60556936960f65e3
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-01-07 23:30:12 +0530

    B

--- B.java
+++ B.java
@@ -1,13 +1,12 @@
-package app_java_5;
+package app_java_8;
 
-public class B extends A{// Super Keyword
-	
-	 public static void main(String[]args) {
-		 B b1 = new B();
-		b1.test();
-	}	
-	public void test () {
-		super.xyz();
+public class B extends A {
+	public static void main(String[] args) {
+		B b1 = new B();
+		b1.test ();
 	}
-}
+	public void test() {
+		System.out.println(A.j);
+	}
 
+}
