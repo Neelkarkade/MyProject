90c2bac7b88a2a16092171a40f60d10c3fa84cd6
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-01-08 23:25:59 +0530

    Exception_ErrorA

---
+++ Exception_ErrorA.java
@@ -0,0 +1,11 @@
+package app_java_8;
+
+public class Exception_ErrorA {
+	public static void main(String[]args) {
+		  int x = 10;
+		  int y = 0;
+		  int z = x/y;    // Stop here  (Exception Accrue) error to solve this  problem use try block and catch block
+		  System.out.println(100);
+	  }
+	}
+
