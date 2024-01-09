f2445ce5b67c6d45762abf75b6b336edbda5d920
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-01-09 23:40:01 +0530

    B

--- B.java
+++ B.java
@@ -1,12 +1,5 @@
-package app_java_8;
+package app_java_17;
 
-public class B extends A {
-	public static void main(String[] args) {
-		B b1 = new B();
-		b1.test ();
-	}
-	public void test() {
-		System.out.println(A.j);
-	}
+public class B {
 
 }
