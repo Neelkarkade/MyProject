7a6dfbc43a58bf5df41d30285e1a41928d667bc4
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-01-09 23:39:15 +0530

    A

--- A.java
+++ A.java
@@ -1,6 +1,11 @@
-package app_java_8;
+package app_java_17;
 
 public class A {
-	static int j = 10;
-
+   public static void main(String[] args) {
+	 A a1 = null;
+	 System.out.println(a1);
+	 
+	 A a2 = new A();
+	 System.out.println(a2);
+}
 }
