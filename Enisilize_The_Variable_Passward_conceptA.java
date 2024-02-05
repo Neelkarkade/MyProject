1bf644af404a5a38b9740b549e97f3cf562af61c
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-02-05 22:51:46 +0530

    Enisilize_The_Variable_Passward_conceptA

---
+++ Enisilize_The_Variable_Passward_conceptA.java
@@ -0,0 +1,18 @@
+package Encapsulation_Concept;
+
+public class Enisilize_The_Variable_Passward_conceptA{
+
+private	 String password;
+
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+	
+}
+	
+	
+}
+
+
