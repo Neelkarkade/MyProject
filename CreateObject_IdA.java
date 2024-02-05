39ac5ffcd7f5f1d19828b7ff31047187924b4e97
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-02-05 22:50:34 +0530

    CreateObject_IdA

---
+++ CreateObject_IdA.java
@@ -0,0 +1,28 @@
+package Encapsulation_Concept;
+
+public class CreateObject_IdA {
+	private int id;
+	private String password;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
