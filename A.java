ec7c1f683bf393270f6111e3c7d77caa337db66c
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-02-08 23:43:10 +0530

    A

--- A.java
+++ A.java
@@ -1,14 +1,20 @@
-package exception_handling;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+package MultiCatchBlock;
+
 public class A {
- public static void main(String[] args) throws FileNotFoundException {
 	
-	
-	 FileReader Fr = new FileReader("G://test.text");
-
-	 
- }
-
-public char[] x;
+	public static void main(String[] args) {
+		try {
+			int i = 10/0;
+		}catch(ArithmeticException a) {
+			System.out.println(a);
+		}catch( NumberFormatException n) {
+		
+		}catch(Exception e) {
+			System.out.println(e);
+		}catch(Throwable t) {
+			
+		}
+	}
 }
+
+
