aa7a0461424fb6554a35f7911d90b639ef88909f
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-02-09 23:37:24 +0530

    UserName

---
+++ UserName.java
@@ -0,0 +1,16 @@
+package revision;
+public class UserName {
+
+	public static void main(String[] args) {
+		try {
+			int x = 10;
+			int y = 0;
+			int z = x/y;
+		}catch(Exception e) {
+			System.out.println(e);
+		
+		}
+		System.out.println(1000);
+	}
+
+}
