57be8a901e54b69a578d262a595879cab1f029c8
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-02-22 22:56:03 +0530

    PostDto

--- PostDto.java
+++ PostDto.java
@@ -1,14 +1,22 @@
-package com.myblog.myblog11.payload;
+package com.payload;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDto {
 
-    private long id;
-    private String tittle;
+    private String postId;
+    private String title;
     private String description;
     private String content;
+
+    List<Comment> comments;
+
+
 }
