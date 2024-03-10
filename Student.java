182b8cf760bac295ecf5299c14e865dd13184c4e
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-03-10 23:30:57 +0530

    Student

--- Student.java
+++ Student.java
@@ -1,60 +1,66 @@
-package com.demo.entity;
+package net.javaguides.sms.entity;
 
-import jakarta.persistence.*;
 
-@Entity //  Will map  his class with  database table Student
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "Student")
 public class Student {
-	@Id //Primary Key
-	@GeneratedValue(strategy = GenerationType.IDENTITY )// Auto increment value
-    private int id;
-	private String name;  
-	private String city;
-	private String course;
-	private int fee;
-	private String email;
-	private long mobile;
 	
-	public String getName() {
-		return name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name",nullable = false)
+	private String firstName;
+	
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+public Student() {
+		
 	}
-	public void setName(String name) {
-		this.name = name;
+	
+	public Student(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
 	}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getCity() {
-	return city;
-}
-public void setCity(String city) {
-	this.city = city;
-}
-public String getCourse() {
-	return course;
-}
-public void setCourse(String course) {
-	this.course = course;
-}
-public int getFee() {
-	return fee;
-}
-public void setFee(int fee) {
-	this.fee = fee;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public long getMobile() {
-	return mobile;
-}
-public void setMobile(long mobile) {
-	this.mobile = mobile;
-}
-
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
