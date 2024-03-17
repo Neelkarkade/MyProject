173f15d28c5116b4fc2303aa5294e35d2c93d52d
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-03-17 23:42:13 +0530

    RegistrationDto

--- RegistrationDto.java
+++ RegistrationDto.java
@@ -2,11 +2,11 @@ package com.webapp.dto;
 
 public class RegistrationDto {
 
-
+   
 	private String firstName;
-	private String lastName;
+	
 	private String email;
-	private long mobile;
+	private String mobile;
 	
 	public String getFirstName() {
 		return firstName;
@@ -14,23 +14,19 @@ public class RegistrationDto {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public long getMobile() {
+	public String getMobile() {
 		return mobile;
 	}
-	public void setMobile(long mobile) {
+	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
+	
 
 }
