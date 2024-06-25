18eabd471651dd54a29adcdc6b58d70eca08db85
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-06-25 23:05:11 +0530

    BusController

--- BusController.java
+++ BusController.java
@@ -1,43 +1,71 @@
-package com.reservationapp.controller;
+package com.app.controller;
 
-import com.reservationapp.entity.Bus;
-import com.reservationapp.paylod.BusDto;
-import com.reservationapp.repository.BusRepository;
-import com.reservationapp.service.BusService;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import com.app.exceptions.BusException;
+import com.app.exceptions.LoginException;
+import com.app.model.Bus;
+import com.app.service.BusService;
 
 @RestController
-@RequestMapping("/api/v1/bus")
 public class BusController {
-    @Autowired
-    private BusService busService;
-
-    //http://localhost:8080/api/v1/bus/add
-
-    @PostMapping("/add")
-    public ResponseEntity<String> addBus(@RequestBody BusDto busDto) throws ParseException {
-
-        busService.addBus(busDto);
-        return new ResponseEntity<>("Bus details added", HttpStatus.CREATED);
-
-
+	@Autowired
+	private BusService  bService;
+		
+	@PostMapping("/save/{key}")
+    public ResponseEntity<Bus> createBus(@Valid @RequestBody Bus bus,@PathVariable("key") String key)throws BusException, LoginException {
+		
+        return new ResponseEntity<Bus>(bService.addBus(bus,key),HttpStatus.OK);
+    } 
+	
+	@GetMapping("/find/{busId}")
+    public ResponseEntity<Bus> getBusById(@PathVariable int busId)throws BusException{
+		
+		return new ResponseEntity<Bus>(bService.viewBus(busId),HttpStatus.OK); 
     }
-        }
+		
+	
+	@PutMapping("/update/{key}")
+    public ResponseEntity<Bus> updateBusById(@Valid @RequestBody Bus bus,@PathVariable("key") String key)throws BusException, LoginException {
+		
+		return new ResponseEntity<Bus>(bService.updateBus(bus,key),HttpStatus.OK);
+    }
+	
+	@DeleteMapping("/delet/{busId}/{key}")
+    public ResponseEntity<Bus> deleteRouteById(@PathVariable int busId,@PathVariable("key") String key)throws BusException, LoginException {
+		
+		return new ResponseEntity<Bus>(bService.deleteBus(busId,key),HttpStatus.OK);
+    }
+	
+	@GetMapping("/buses/{busType}")
+	public ResponseEntity<List<Bus>> getBusesByType(@PathVariable("busType") String busType) throws BusException{
+		
+		List<Bus> listOfBuses=bService.viewBusByType(busType);
+		
+		return new ResponseEntity<List<Bus>>(listOfBuses,HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/buses")
+	public ResponseEntity<List<Bus>> getAllBuses() throws BusException{
+		
+		List<Bus> listOfBuses=bService.viewAllBuses();
+		
+		return new ResponseEntity<List<Bus>>(listOfBuses,HttpStatus.OK);
+	}
 
-
-
-
-
-
-
-
+}
