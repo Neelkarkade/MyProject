41c681ea48c29938caf013044783d9aaef0940cb
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-06-30 23:16:17 +0530

    SubRoute

---
+++ SubRoute.java
@@ -0,0 +1,25 @@
+package com.reservationapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "sub_route")
+public class SubRoute {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fromLocation;
+    private String toLocation;
+    private String fromDate;
+    private String toDate;
+    private String totalDuration;
+    private String fromTime;
+    private String toTime;
+    private Route route;
+}
