860e50b3bf7c39d07edb6596ee1af38029465dc2
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-07-13 23:39:51 +0530

    UserRegistrationDto

---
+++ UserRegistrationDto.java
@@ -0,0 +1,13 @@
+package com.reservationapp.paylod;
+
+import jakarta.persistence.Lob;
+
+public class UserRegistrationDto {
+
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private byte[] profilePicture;
+
+}
