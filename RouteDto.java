2df56cf3213a4c020a971e0425c2d813d6ab8684
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-07-13 23:37:02 +0530

    RouteDto

---
+++ RouteDto.java
@@ -0,0 +1,27 @@
+package com.reservationapp.paylod;
+
+import com.reservationapp.entity.Bus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RouteDto {
+
+    private String fromLocation;
+    private String toLocation;
+    private String fromDate;
+    private String toDate;
+    private String totalDuration;
+    private String fromTime;
+    private String toTime;
+
+
+
+
+}
