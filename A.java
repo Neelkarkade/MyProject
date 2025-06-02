3f8eb57d199abb6614111f11b00c8cbe1be29598
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-06-02 22:27:50 +0530

    A

--- A.java
+++ A.java
@@ -1,20 +1,8 @@
-package MultiCatchBlock;
-
+package app_javaPratice_1;
 public class A {
-	
+	 int employeeId = 10;
 	public static void main(String[] args) {
-		try {
-			int i = 10/0;
-		}catch(ArithmeticException a) {
-			System.out.println(a);
-		}catch( NumberFormatException n) {
+int currentDate;
 		
-		}catch(Exception e) {
-			System.out.println(e);
-		}catch(Throwable t) {
-			
-		}
 	}
 }
-
-
