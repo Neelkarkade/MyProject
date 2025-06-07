371083cead97a022ee24b4ba287bb6b046cb368e
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-06-07 21:41:28 +0530

    A1

---
+++ A1.java
@@ -0,0 +1,15 @@
+package app_javaPratice_17;
+
+public class A1 {
+	static A1 a1 = new A1();
+	public static void main(String[] args) {
+		System.out.println(a1);
+		a1.test();
+	}
+	
+	public void test() {
+		System.out.println(a1);
+		
+	}
+
+}
