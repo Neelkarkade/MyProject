4126ebb1a9af2ad6bcd37106be57b0a9af70133c
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-06-07 21:42:11 +0530

    A2

--- A2.java
+++ A2.java
@@ -1,9 +1,19 @@
-package app_javaPratice_1;
+package app_javaPratice_17;
+
+
 
 public class A2 {
-int x;
-public static void main(String[] args) {
-	A2 a1 = new A2();
-	System.out.println(a1.x);
-}
+	public static void main(String[] args) {
+		A2 a1 = new A2();
+		 a1.test(100,200,300);
+		
+		
+	}
+	
+	public void test(int...x) { 
+    System.out.println(x[0]);	
+    System.out.println(x[1]);
+    System.out.println(x[2]);
+	}
+	
 }
