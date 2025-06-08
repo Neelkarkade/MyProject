d92d5b2415d2947919da6e4b64e1194e2fb7d752
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-06-09 05:29:16 +0530

    CountNumberlengthOfCharacter

---
+++ CountNumberlengthOfCharacter.java
@@ -0,0 +1,12 @@
+package p1;
+
+import java.io.File;
+
+public class CountNumberlengthOfCharacter {
+	public static void main(String[] args) {
+		File f = new File("C:\\test\\A.txt");
+		long val = f.length();
+		System.out.println(val);
+		}
+	}
+
