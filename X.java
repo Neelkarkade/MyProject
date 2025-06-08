f0370e9d55ca0b34cc548367de50631b0ba08c0f
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-06-08 23:12:44 +0530

    X

---
+++ X.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class X {
+	public static void main(String[] args) {
+		String x = "madam";
+		System.out.println(x.charAt(2));
+	    System.out.println(x.length());
+	   
+	}
+
+}
