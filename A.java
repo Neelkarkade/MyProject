7f16b8f3728310da27d24cf0b58bd791b77eacab
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-20 22:22:30 +0530

    A

--- A.java
+++ A.java
@@ -1,12 +1,8 @@
-package app_javaPratice_17;
+package app_javaPratice_20;
 
 public class A {
 	public static void main(String[] args) {
-		A a1 = null;
-		System.out.println(a1);
-		
-		A a2 = new A();
-		System.out.println(a2);
+		System.out.println(10);
 	}
 
 }
