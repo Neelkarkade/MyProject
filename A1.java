9a98dbd1a3d5f4c670e2690a91d4d9420b07eccc
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-20 22:22:51 +0530

    A1

--- A1.java
+++ A1.java
@@ -1,15 +1,5 @@
-package app_javaPratice_17;
+package app_javaPratice_20;
 
 public class A1 {
-	static A1 a1 = new A1();
-	public static void main(String[] args) {
-		System.out.println(a1);
-		a1.test();
-	}
-	
-	public void test() {
-		System.out.println(a1);
-		
-	}
 
 }
