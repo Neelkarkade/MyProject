903a97c5d14810dfca28f57e98422fa4555f0072
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-29 23:33:54 +0530

    A_StoreNamesInArray

---
+++ A_StoreNamesInArray.java
@@ -0,0 +1,15 @@
+package app_javaPratice_17;
+
+public class A_StoreNamesInArray {
+public static void main(String[] args) {
+
+	String[] names = new String [3];
+	names[0] ="mike";
+	names[1] = "stallin";
+    names[2] = "adam";
+	
+    System.out.println(names[0]);
+    System.out.println(names[1]);
+    System.out.println(names[2]);
+}
+}
