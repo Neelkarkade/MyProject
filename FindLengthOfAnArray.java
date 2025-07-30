650cd469092e5663e5df424d5f4761d77c7a1e59
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-30 22:11:47 +0530

    FindLengthOfAnArray

---
+++ FindLengthOfAnArray.java
@@ -0,0 +1,10 @@
+package app_javaPratice_17;
+
+public class FindLengthOfAnArray {
+
+	public static void main(String[] args) {
+		String[] names = new String[300];
+		System.out.println(names.length);
+	}
+
+}
