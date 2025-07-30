4e905aed9737a2c2d66b4154649d0f75bfb6f8ff
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-30 22:12:24 +0530

    GreaterThanEqualTwo

---
+++ GreaterThanEqualTwo.java
@@ -0,0 +1,14 @@
+package app_javaPratice_17;
+
+public class GreaterThanEqualTwo {
+	public static void main(String[] args) {
+		
+		System.out.println(1==2);
+		System.out.println(1<2);
+		System.out.println(1>2);
+		System.out.println(1!=2);
+		System.out.println(1>=1);
+		System.out.println(1<=2);
+	}
+
+}
