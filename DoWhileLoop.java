d5cbdd59e6da10cf31c74577d5f69691ba4c717b
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-30 22:11:10 +0530

    DoWhileLoop

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+package app_javaPratice_17;
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		int x = 3;
+		do {
+			System.out.println(x);//in DowhileLoop while first time runing condition is false then first it will run and secound time when code run it will check condition if false stop the runing code
+			x--;
+			
+		}while(x>0);
+	}
+
+}
