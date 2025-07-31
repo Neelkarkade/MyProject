11a9151a789b18da189e924bc86b4cab242973bd
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-31 23:20:30 +0530

    SwitchCase

---
+++ SwitchCase.java
@@ -0,0 +1,23 @@
+package app_javaPratice_17;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int key = 1;
+		switch (key) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		case 3:
+			System.out.println(3);
+			break;
+
+		default:
+			System.out.println("No match Found");
+			break;
+		}
+	}
+
+}
