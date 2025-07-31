704ca7a3db11976f014abcee9e4a67dd02528887
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-07-31 23:21:53 +0530

    WhileLoop

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+package app_javaPratice_17;
+
+public class WhileLoop {
+public static void main(String[] args) {
+	int x = 0;
+	while(x<3) {// whitout (x++) it will Infinite Loop
+	System.out.println(x);
+	x++;
+	
+	}
+	
+}
+}
