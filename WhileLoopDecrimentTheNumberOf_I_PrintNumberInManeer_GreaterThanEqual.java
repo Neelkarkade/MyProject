0e2cc19fde0f4c0dc512782a9e45f067f1dc8ff3
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-01 23:04:37 +0530

    WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer_GreaterThanEqual

---
+++ WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer_GreaterThanEqual.java
@@ -0,0 +1,9 @@
+package app_javaPratice_17;
+
+public class WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer_GreaterThanEqual {
+public static void main(String[] args) {
+	for(int i=5;i>=0;i--) {//5 to 0
+		System.out.println(i);
+	}
+}
+}
