02eabb93a5585800b42d6a6d580e7b386fe2315b
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-01 23:04:55 +0530

    WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer

---
+++ WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer.java
@@ -0,0 +1,9 @@
+package app_javaPratice_17;
+
+public class WhileLoopDecrimentTheNumberOf_I_PrintNumberInManeer {
+public static void main(String[] args) {
+	for(int i=5;i>0;i--){// 5 to 1
+		System.out.println(i);
+	}
+}
+}
