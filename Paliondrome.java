2c59e74fbc2c17c42cad5be39b8d016717e8a4c1
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-03 22:13:06 +0530

    Paliondrome

---
+++ Paliondrome.java
@@ -0,0 +1,10 @@
+package app_javaPratice_17;
+
+public class Paliondrome {
+	public static void main(String[] args) {
+		String x = "madam";
+		System.out.println(x.charAt(2));
+		System.out.println(x.length());
+	}
+
+}
