cdb0c541f0a8162a6bb051af9c8f87cc4f6d3139
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-04 23:05:16 +0530

    B

--- B.java
+++ B.java
@@ -1,9 +1,22 @@
-package p2;
+package app_javaPratice_17;
 
-import p1.A;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
-public class B {
-public static void main(String[]args){
-A a1 = new A();
-}
+
+
+public class B {//FileToBinaryNumberToObject
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("C://test//H.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+	        A a1 = (A)ois.readObject();
+			System.out.println(a1.name);
+			System.out.println(a1.password);
+			
+		} catch (Exception e) {
+           e.printStackTrace();		
+           }
+	}
+
 }
