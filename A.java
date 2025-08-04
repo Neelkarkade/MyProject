43f7b565f01b9a33928928273931bfe3801bb4b9
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-04 23:04:56 +0530

    A

--- A.java
+++ A.java
@@ -1,8 +1,9 @@
-package app_javaPratice_20;
+package app_javaPratice_17;
 
-public class A {
-	public static void main(String[] args) {
-		System.out.println(10);
-	}
+import java.io.Serializable;
+
+public class A implements Serializable{
+	public String name="mike";//ObjectToBinaryNumberToFileA2,FileToBinaryNumberToObjectB2
+	public String password="testing";
 
 }
