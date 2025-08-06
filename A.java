73c77852b5e6fc8547bd27046d7bce76734781ab
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-06 22:02:04 +0530

    A

--- A.java
+++ A.java
@@ -1,8 +1,6 @@
-package BuildingMethodWhenTheWrittenTypeIsInt;
+package IfInterfaceHaveTwoMethodAndClassHaveToDevelope;
 
 public interface A {
-public static void main(String[] args) {// If you make a variable final the re initialization is not allowed
-	final int x = 10;
-	x = 10;// Error
-}
+	public void test1();
+    public void test2();
 }
