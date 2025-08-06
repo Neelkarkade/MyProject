b3588843180b78ec8c942aaed40917aee6d0dc73
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-06 22:00:19 +0530

    A11

---
+++ A11.java
@@ -0,0 +1,7 @@
+package BuildingMethodWhenTheWrittenTypeIsInt;
+
+public interface A11 {
+	int AMOUNT=100;  //  All variable by default in an interface are final and static
+	final static int CURRENT_YEAR=2024;
+
+}
