708c0666dc9bfdfebab01df84e44066dde98e681
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-06 21:59:59 +0530

    A2

--- A2.java
+++ A2.java
@@ -1,19 +1,5 @@
-package app_javaPratice_17;
+package BuildingMethodWhenTheWrittenTypeIsInt;
 
+final public class A2 {
 
-
-public class A2 {
-	public static void main(String[] args) {
-		A2 a1 = new A2();
-		 a1.test(100,200,300);
-		
-		
-	}
-	
-	public void test(int...x) { 
-    System.out.println(x[0]);	
-    System.out.println(x[1]);
-    System.out.println(x[2]);
-	}
-	
 }
