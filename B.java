4ade62a3b39b2370f6f9e50e09ed38ad1ae65aeb
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-06 22:00:48 +0530

    B

--- B.java
+++ B.java
@@ -1,22 +1,12 @@
-package app_javaPratice_17;
+package BuildingMethodWhenTheWrittenTypeIsInt;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+public class B {
+final int x;
+final static int y;  //  If you make static/ non static variable final then it is mandatory to initialize it or else you will get blank final field
 
-
-
-public class B {//FileToBinaryNumberToObject
-	public static void main(String[] args) {
-		try {
-			FileInputStream fis = new FileInputStream("C://test//H.ser");
-			ObjectInputStream ois = new ObjectInputStream(fis);
-	        A a1 = (A)ois.readObject();
-			System.out.println(a1.name);
-			System.out.println(a1.password);
-			
-		} catch (Exception e) {
-           e.printStackTrace();		
-           }
-	}
+public static void main(String[] args) {  
+	
+}
+	
 
 }
