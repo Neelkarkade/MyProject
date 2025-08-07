d4790b6ed5c09a239eef0dab47a09c5a183ec3f2
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-07 21:54:54 +0530

    B

--- B.java
+++ B.java
@@ -1,7 +1,13 @@
-package MultiLevelInheritance;
+package LamdaExpresion;
 
-public interface B extends A {
-	public void test2();
-	
+public class B {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        A a1 = ()->{               //  LamBdas Expression It helps us to reduce the number of line of code in java
+		System.out.println(1);
+		};
+        a1.test1();
+
+	}
 
 }
