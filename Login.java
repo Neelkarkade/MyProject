92656cd69bc8f6839c5458c18c6c2df01549df94
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-09 22:56:24 +0530

    Login

---
+++ Login.java
@@ -0,0 +1,46 @@
+package p1;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/verifyLogin")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Login() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		 try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/augdb","root","test");
+				Statement stmnt= con.createStatement();
+				ResultSet result = stmnt.executeQuery("Select * from login where email='"+email+"' and password='"+password+"'");
+			    if(result.next()) {
+				  System.out.println("Welcome");
+				}else {
+				  System.out.println("Invalid username/password");
+}
+} catch (Exception e) {
+			       e.printStackTrace();
+			    }
+	}
+
+}
