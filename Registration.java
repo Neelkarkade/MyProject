68b65ba7526979cbaff6fbd5cb29a759cfe6a43e
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-09 22:57:17 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -1,68 +1,47 @@
-package com.webapp.entity;
+package p1;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Entity
-@Table(name="registrations")
-public class Registration {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+@WebServlet("/reg")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public Registration() {
+        super();
+    }
+   @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("do Get");
 
-private long id;
-	@NotEmpty
-	@Size(min = 2,message = "minmumm 2 charcter")
-@Column(name ="first_name",length = 45)
-private String firstName;//first_name
-	
-@Column(name ="lastName",length = 45)
-private String lastName;
+	}
+   @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+String name = request.getParameter("firstName");	
+String email = request.getParameter("emailId");
+String mobile = request.getParameter("mobile");
+System.out.println(name);
+System.out.println(email);
+System.out.println(mobile);
+  try {
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/augdb","root","test");
+	         Statement stmnt= con.createStatement();
+	         stmnt.executeUpdate("insert into registration values ('"+name+"','"+email+"','"+mobile+"')");
+             con.close();
+  } catch (Exception e) {
+       e.printStackTrace();
+    }
 
-@Email
-@Column(name ="email",unique = true,  length = 128)
-private String email;
-
-@Size(min = 10, max = 10, message = "Should be 10 digits")
-@Column(name = "mobile")
-private String mobile;
-
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public @Digits(integer = 10, fraction = 0, message = "Mobile number must be exactly 10 digits") String getMobile() {
-	return mobile;
-}
-public void setMobile(String mobile) {
-	this.mobile = mobile;
-}
-
+   }
 
 }
