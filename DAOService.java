80aed47e4e884e131fdf399f05d123a9165dac47
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-11 22:45:34 +0530

    DAOService

--- DAOService.java
+++ DAOService.java
@@ -1,8 +1,14 @@
-package comm.registrationapp.model;
+package com.studentapp.model;
+
+import java.sql.ResultSet;
 
 public interface DAOService {
+	
 	public void connectDB();
-public boolean verifyLogin(String email, String password);
-public void saveRegistration(String name,String city,String email,String mobile);
-public void updateRegistration(String email, String mobile); 
+	public boolean verifyLogin(String email, String password);
+	public void createInquiry(String name,String email, String mobile);
+	public void deleteInquiry(String email);
+	public void updateInquiry();
+	public ResultSet listInquiry();
+
 }
