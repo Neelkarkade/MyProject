3731eec970f1ef3591ed1aa7129e2346d147cb07
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-11 22:45:54 +0530

    DAOServiceImpl

--- DAOServiceImpl.java
+++ DAOServiceImpl.java
@@ -1,71 +1,59 @@
-package comm.registrationapp.model;
+package com.studentapp.model;
 
 import java.sql.*;
 
 public class DAOServiceImpl implements DAOService {
-	private Connection con;
-	private Statement stmnt;
-
+  private Connection con;
+  private Statement stmnt;
+  
+  
 	@Override
 	public void connectDB() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/reg_db_app", "root", "test");
-			stmnt = con.createStatement();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+      try {
+    	  Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/aug_db","root","test");
+		stmnt  = con.createStatement();
+	} catch (Exception e) {
+    e.printStackTrace();
+	}
+	}
+
+	@Override
+	public void createInquiry(String name, String email, String mobile) {
+      try {
+    	  stmnt.executeUpdate("insert into inquiry values('"+name+"','"+email+"','"+mobile+"')");
+	} catch (Exception e) {
+         e.printStackTrace();
+	}
+	}
+
+	@Override
+	public void deleteInquiry(String email) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateInquiry() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ResultSet listInquiry() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean verifyLogin(String email, String password) {
 		try {
-			ResultSet result = stmnt
-					.executeQuery("select * from login where email='"+email+"' and password='"+password+"'");
-			return result.next();
+			ResultSet result = stmnt.executeQuery("Select * from login where email='"+email+"'and password='"+password+"'");
+		    return result.next();
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	@Override
-	public void saveRegistration(String name, String city, String email, String mobile) {
-		try {
-		 stmnt.executeUpdate("insert into registration values('"+name+"','"+city+"','"+email+"','"+mobile+"')");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public ResultSet readAllReg() {
-		try {
-			ResultSet result = stmnt
-					.executeQuery("select * from registration");
-			return result;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		
-		}
-		return  null;
-	}
-
-	public void deleteByEmail(String email) {
-		try {
-			stmnt.executeUpdate("Delete From registration where email='"+email+"'");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	public void updateRegistration(String email, String mobile) {
-		try {
-			stmnt.executeUpdate("Update registration set mobile='"+mobile+"'where email='"+email+"'");
-		} catch (Exception e) {
-			e.printStackTrace();
+		 e.printStackTrace();
 		}		
+return false;
 	}
 
 }
