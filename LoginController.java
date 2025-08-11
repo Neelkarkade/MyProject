4e54839a6f0e8eef1426cdb461c7d3e55f838dc4
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-11 22:45:12 +0530

    LoginController

--- LoginController.java
+++ LoginController.java
@@ -1,54 +1,47 @@
-package loginp1;
+package com.studentapp.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.studentapp.model.DAOServiceImpl;
+
+
 @WebServlet("/verifyLogin")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+       
+   
     public LoginController() {
         super();
+
     }
 
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		
-		PrintWriter out = response.getWriter();
-		
-		//JDBC Code
-		
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/login_db_v1","root","test");
-			Statement stmt=con.createStatement();
-			ResultSet result = stmt.executeQuery("select * from login where email='"+email+"' and password='"+password+"'");
-			if(result.next()) {
-				out.println("Welcome");
-		}else {
-			out.println("invalid username/password");
-		}
-			con.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+       String email = request.getParameter("email");
+       String password = request.getParameter("password");
+       DAOServiceImpl service = new DAOServiceImpl();
+       service.connectDB();
+       boolean status = service.verifyLogin(email, password);
+       if(status) {
+    	   RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/inquiry.jsp");
+    	   rd.forward(request, response);
+       }else {
+    	   request.setAttribute("errorMessage", "invalid credentials");
+    	   RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+    	   rd.forward(request, response);
+       }
 	}
 
 }
