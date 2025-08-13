5157418a701e9c969b37796188abfd3f25e945d0
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-13 16:16:26 +0530

    InquiryController

--- InquiryController.java
+++ InquiryController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.studentapp.model.DAOServiceImpl;
 
@@ -23,22 +24,39 @@ public class InquiryController extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/inquiry.jsp");
+  	     rd.forward(request, response);
 
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 String name = request.getParameter("name");
-	     String email = request.getParameter("email");
-	     String mobile = request.getParameter("mobile");
-	     DAOServiceImpl service = new DAOServiceImpl();
-	     service.connectDB();
-	     service.createInquiry(name, email, mobile);
-  	     
-	     request.setAttribute("message", "saved!!");
-	     RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/inquiry.jsp");
-  	     rd.forward(request, response);
+		 
+		try {
+			HttpSession session = request.getSession(false);
+			if(session.getAttribute("email")!=null) {
+				 String name = request.getParameter("name");
+			     String email = request.getParameter("email");
+			     String mobile = request.getParameter("mobile");
+			     DAOServiceImpl service = new DAOServiceImpl();
+			     service.connectDB();
+			     service.createInquiry(name, email, mobile);
+	  	     
+		     request.setAttribute("message", "saved!!");
+		     RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/inquiry.jsp");
+	  	     rd.forward(request, response);
+			}else {
+				 RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		  	     rd.forward(request, response);
 
+			}
+		} catch (NullPointerException e) {
+                 e.printStackTrace();	
+    		     request.setAttribute("errorMessage", "Session time out. login again...");
+                 RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		  	     rd.forward(request, response);
+
+		}
 	}
 
 }
