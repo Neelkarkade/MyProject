d5f6b8291eab9d2695df88ad98758c2147400984
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-18 22:59:11 +0530

    DemoeeApplication

---
+++ DemoeeApplication.java
@@ -0,0 +1,13 @@
+package com.demoee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoeeApplication.class, args);
+	}
+
+}
