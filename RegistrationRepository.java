4d34ea3d5af0c63cc9db52d37927ead80ad3e603
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-19 23:07:55 +0530

    RegistrationRepository

--- RegistrationRepository.java
+++ RegistrationRepository.java
@@ -1,22 +1,32 @@
 package com.demoee.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.demoee.entity.Registration;
 
 public interface RegistrationRepository extends CrudRepository<Registration, Long>{
 
-	
-	
-	
-	Optional<Registration> findByEmail(String email);
+	//Finder Method Should be abstract
+	//Should start with findByVariablename
+	@Query("Select r from Registration r where r.email=:x")
+	Registration findByEmployeeEmail(@Param("x")String email);	
 	
 	Optional<Registration> findByMobile(String mobile);
 	
-    Iterable<Registration> findByEmailOrMobile(String email,String mobile);
-
-    Optional<Registration> findByEmailAndMobile(String email,String mobile);
+	@Query("Select r from Registration r where r.email=:x or r.mobile=:y")
+    List<Registration> findByEmailOrMobile(
+    		@Param("x") String email,
+    		@Param("y") String mobile
+    				);
+    @Query("Select r from Registration r where r.email=:x and r.mobile=:y")
+    Registration findByEmailAndMobile(
+    		@Param("x")String email,
+    		@Param("y")String mobile
+    		);
 
 }
