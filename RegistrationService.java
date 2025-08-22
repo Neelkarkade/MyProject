f7e9736adc0337e7a7973bb97fe718c88a7c7b86
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-22 23:09:40 +0530

    RegistrationService

--- RegistrationService.java
+++ RegistrationService.java
@@ -1,36 +1,21 @@
-package com.webapp.service;
-
-import java.util.List;
+package com.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.webapp.entity.Registration;
-import com.webapp.repository.RegistrationRepository;
+import com.demo.entity.Registration;
+import com.demo.repository.RegistrationRepository;
 
 @Service
 public class RegistrationService {
-@Autowired
-private RegistrationRepository registrationRepository;
 
-public void saveRegistration(Registration registration) {
-	registrationRepository.save(registration);
-}
-
-public List<Registration> getAllRegistrations() {
-	List<Registration> regs = registrationRepository.findAll();
-	return regs;
-}
-
-public void deleteRegById(long id) {
-registrationRepository.deleteById(id);	
-}
-
-public Registration getRegistrationById(long id) {
-	Registration reg = registrationRepository.findById(id).get();
-	return reg;
-}
-
-
+	@Autowired
+	private RegistrationRepository registrationRepository;
+	
+  public void saveRegistration(Registration registration) {
+	  registrationRepository.save(registration);
+	  
+	  
+  }
 
 }
