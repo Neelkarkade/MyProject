52ca904f60f5dbdac4987a6a5c8b210f63c7d005
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-22 23:08:26 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -1,44 +1,56 @@
-package com.demoee.entity;
+package com.demo.entity;
 
-
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 @Entity
-
+@Table(name = "registrations")
 public class Registration {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private long  id;
+	
+	@Column(name="name", nullable = false, length = 45)
 	private String name;
-	private String email;
+	
+	@Column(name="email_id", nullable = false, length = 128, unique = true)
+	private String emailId;
+	
+	@Column(name="mobile", nullable = false, length = 10, unique = true)
 	private String mobile;
-	
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getEmail() {
-		return email;
+
+	public String getEmailId() {
+		return emailId;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
+
 	public String getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
 	
-	
 
+	
 }
