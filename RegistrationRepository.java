e17cb3a2f0420d941bdd688f3ae6501832d51318
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-26 21:14:20 +0530

    RegistrationRepository

--- RegistrationRepository.java
+++ RegistrationRepository.java
@@ -1,9 +1,9 @@
-package com.demo.repository;
+package com.exampleapi.repository;
 
+import com.exampleapi.entity.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.demo.entity.Registration;
-
+@Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-
 }
