87a0254881e20cd1fdd009315182a95365f79f41
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-26 21:14:50 +0530

    RegistrationService

--- RegistrationService.java
+++ RegistrationService.java
@@ -1,21 +1,59 @@
-package com.demo.service;
+package com.exampleapi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.exampleapi.entity.Registration;
+
+import com.exampleapi.payload.RegistrationDto;
+import com.exampleapi.repository.RegistrationRepository;
 import org.springframework.stereotype.Service;
 
-import com.demo.entity.Registration;
-import com.demo.repository.RegistrationRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RegistrationService {
+    private RegistrationRepository registrationRepository;
 
-	@Autowired
-	private RegistrationRepository registrationRepository;
-	
-  public void saveRegistration(Registration registration) {
-	  registrationRepository.save(registration);
-	  
-	  
-  }
+    public RegistrationService(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
 
+    public RegistrationDto saveRegistration(RegistrationDto registrationDto) {
+        //Convert to Entity From Dto
+        Registration registration = new Registration();
+        registration.setName(registrationDto.getName());
+        registration.setEmailId(registrationDto.getEmail());
+        registration.setMobile(registrationDto.getMobile());
+        Registration savedReg = registrationRepository.save(registration);
+        //Convert entity to dto
+        RegistrationDto savedRegDto = new RegistrationDto();
+        savedRegDto.setName(savedRegDto.getName());
+        savedRegDto.setEmail(savedRegDto.getMobile());
+        savedRegDto.setMobile(savedRegDto.getMobile());
+        return savedRegDto;
+    }
+
+    public void deleteRegistration(long id) {
+        registrationRepository.deleteById(id);
+    }
+
+    public void updateRegistration(long id, Registration registration) {
+        //Actual Record fetched from database
+        Optional<Registration> opReg = registrationRepository.findById(id);
+        Registration reg = opReg.get();
+        reg.setName(registration.getName());
+        reg.setEmailId(registration.getEmailId());
+        reg.setMobile(registration.getMobile());
+        registrationRepository.save(reg);
+    }
+
+    public List<Registration> getAllregistrations() {
+        List<Registration> registrations = registrationRepository.findAll();
+        return registrations;
+    }
+
+    public Registration getRegistrationById(long id) {
+        Optional<Registration> opReg = registrationRepository.findById(id);
+        Registration reg = opReg.get();
+        return reg;
+    }
 }
