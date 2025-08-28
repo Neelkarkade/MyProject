09e602abde53a3389cb3bd13842727f11845247c
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-08-28 23:16:17 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -11,16 +11,16 @@ import lombok.Setter;
 public class Registration {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 1000)
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @Column(name = "email_id", nullable = true)
+    @Column(name = "email_id", nullable = false, unique = true)
     private String emailId;
 
-    @Column(name = "mobile", nullable = false, length = 10)
+    @Column(name = "mobile", nullable = false, length = 10, unique = true)
     private String mobile;
 
     public Long getId() {
