73b8f30b6cfa434b8a114d6a76a19504ca8db180
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-04 23:45:02 +0530

    Video

---
+++ Video.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "video")
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "url", nullable = false, length = 500)
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
