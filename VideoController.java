60e22aec54a1e9e6eebda513428f426c2f70c07f
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-05 23:35:28 +0530

    VideoController

--- VideoController.java
+++ VideoController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.Video;
 import com.example.demo.repository.VideoRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/video")
@@ -25,6 +22,12 @@ public class VideoController {
     ){
         videoRepository.save(video);
         return ResponseEntity.ok(video);
+    }
+    @DeleteMapping
+    public void deleteVideo(
+            @RequestParam long id
+    ){
+        videoRepository.deleteById(id);
     }
 
 
