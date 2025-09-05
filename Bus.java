da5b7d4c5e83d4422e9d64eb316599b8d523309b
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-05 23:35:51 +0530

    Bus

--- Bus.java
+++ Bus.java
@@ -1,30 +1,32 @@
-package com.reservationapp.entity;
-
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import org.springframework.util.RouteMatcher;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
 @Entity
+@Table(name = "bus")
 public class Bus {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long busId;
-    private String busNumber;
-    private String busType;
-    private double price;
-    private int totalSeats;
-    private int availableSeats;
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-    @OneToOne
-    private Route route;
+    @Column(name = "name", nullable = false)
+    private String name;
 
-    // Constructors, getters, and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
