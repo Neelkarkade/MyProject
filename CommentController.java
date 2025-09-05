4d968ca467c5f99952a7e5a1830dbf4052303a50
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-05 23:34:36 +0530

    CommentController

--- CommentController.java
+++ CommentController.java
@@ -4,8 +4,11 @@ import com.example.demo.entity.Comments;
 import com.example.demo.entity.Video;
 import com.example.demo.repository.CommentsRepository;
 import com.example.demo.repository.VideoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/comment")
@@ -23,12 +26,21 @@ public class CommentController {
 
    @PostMapping
     public ResponseEntity<Comments> createComment(
-            @RequestBody Comments comment,
-            @RequestParam long videoId
+            @RequestBody Comments comment
     ){
 
-       Video video = videoRepository.findById(videoId).get();
-       
+       commentsRepository.save(comment);
+       return  new ResponseEntity<>(comment, HttpStatus.CREATED);
    }
+
+   @GetMapping
+    public ResponseEntity<List<Comments>> getAllComments(
+            @RequestParam Long videoId
+   ){
+        Video video = videoRepository.findById(videoId).get();
+        List<Comments> comments = commentsRepository.findByVideo(video);
+        return  new ResponseEntity<>(comments, HttpStatus.OK);
+
+    }
 
 }
