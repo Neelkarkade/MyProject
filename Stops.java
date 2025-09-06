41a2ef905ceb7b795084182ff77265b32c774155
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-06 08:08:40 +0530

    Stops

---
+++ Stops.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "stops")
+public class Stops {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
