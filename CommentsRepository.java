58a0ff81214f2b28b3875a14da16cfb9199b3ce6
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-11 22:30:43 +0530

    CommentsRepository

--- CommentsRepository.java
+++ CommentsRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Comments;
+import com.example.demo.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
+
+    List<Comments> findByVideo(Video video);
 }
