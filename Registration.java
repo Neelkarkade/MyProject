e6be22fd50563428f2677d6b5b05b08c9a7342f1
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-13 22:17:08 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -1,57 +1,47 @@
-package com.exampleapi.entity;
+package p1;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-@Getter
-@Setter
-@Entity
-@Table(name = "registrations")
-public class Registration {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-    @Column(name = "name", nullable = false, length = 100)
-    private String name;
+@WebServlet("/reg")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public Registration() {
+        super();
+    }
+   @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("do Get");
 
-    @Column(name = "email_id", nullable = false, unique = true)
-    private String emailId;
-
-    @Column(name = "mobile", nullable = false, length = 10, unique = true)
-    private String mobile;
-
-    public Long getId() {
-        return id;
+	}
+   @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+String name = request.getParameter("firstName");	
+String email = request.getParameter("emailId");
+String mobile = request.getParameter("mobile");
+System.out.println(name);
+System.out.println(email);
+System.out.println(mobile);
+  try {
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/augdb","root","test");
+	         Statement stmnt= con.createStatement();
+	         stmnt.executeUpdate("insert into registration values ('"+name+"','"+email+"','"+mobile+"')");
+             con.close();
+  } catch (Exception e) {
+       e.printStackTrace();
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+   }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
 }
