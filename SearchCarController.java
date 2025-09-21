9ef82cce20c140929c69ca1061463e7d2b118624
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-09-21 22:01:39 +0530

    SearchCarController

---
+++ SearchCarController.java
@@ -0,0 +1,32 @@
+package com.app.controller;
+
+
+import com.app.entity.cars.Brand;
+import com.app.entity.cars.Car;
+import com.app.repository.BrandRepository;
+import com.app.repository.CarRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.CacheRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/search-car")
+public class SearchCarController {
+
+    private final BrandRepository brandRepository;
+    private CarRepository carRepository;
+
+    public SearchCarController(BrandRepository brandRepository, CarRepository carRepository) {
+        this.brandRepository = brandRepository;
+        this.carRepository = carRepository;
+    }
+
+    //http://localhost:8080/api/v1/search-car/cars?brand=honda
+    @GetMapping("/cars")
+    public List<Car> searchCars(
+            @RequestParam String param
+    ){
+        return carRepository.searchCar(param);
+    }
+}
