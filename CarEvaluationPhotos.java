97822abdc474b5fcc683dccdbe6581e2fe4f417d
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-10-11 21:53:44 +0530

    CarEvaluationPhotos

---
+++ CarEvaluationPhotos.java
@@ -0,0 +1,24 @@
+package com.app.entity.evalution;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "car_evaluation_photos")
+public class CarEvaluationPhotos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "car_detailed_evaluation_id")
+    private CarDetailedEvaluation carDetailedEvaluation;
+
+}
