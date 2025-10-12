9d13781a72fd92eea4c3a70090e0b59b9154af85
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2025-10-12 22:59:28 +0530

    LoginDto

--- LoginDto.java
+++ LoginDto.java
@@ -1,24 +1,23 @@
-package com.myblog.myblog11;
+package com.app.payload;
 
 public class LoginDto {
-    private String userName;
-    private String password;
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    private String  username;
+    private  String password;
+
+    public String getUsername() {
+        return username;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-
-    public String getUserName() {
-        return userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
