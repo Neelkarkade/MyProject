dd728c73dce7017e665ca90ea6a9d4ef7f9ad7bd
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2026-06-18 00:04:35 +0530

    CommentRepository

--- CommentRepository.java
+++ CommentRepository.java
@@ -1,6 +1,6 @@
-package com.microservice.comment.repository;
+package com.micriservice.comment.repository;
 
-import com.microservice.comment.entity.Comment;
+import com.micriservice.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +8,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment,String> {
 
     List<Comment> findByPostId(String postId);
+
 }
